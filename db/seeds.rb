9236d8f2e2e68a0aee2c936159c77d6c3b7c56b9
John Hennerich <john.hennerich@gmail.com>
2022-07-10 15:35:50 -0600

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -12,25 +13,22 @@ User.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('users')
 
 user = User.create!(first_name: 'John', last_name: 'H', email: 'John.H@email.com',
-  sms: '5551234567', notify: 3)
+                    sms: '5551234567', notify: 3)
 
- Prescription.create!(user_id: user.id, med_name: 'Tylenol', time_of_last_dose: Time.current,
-   time_of_next_dose: 4.hours.from_now, total_doses: 500, doses_remaining: 500,
-   max_daily_doses: 6, user_instructions: "['take pill', 'take with food']",
-   doctor_instructions: 'take 2 call me in the morning', time_between_dose: 240,
-   icon: 'path_to_icon'
-   )
+Prescription.create!(user_id: user.id, med_name: 'Tylenol', time_of_last_dose: Time.current,
+                     time_of_next_dose: 4.hours.from_now, total_doses: 500, doses_remaining: 500,
+                     max_daily_doses: 6, user_instructions: "['take pill', 'take with food']",
+                     doctor_instructions: 'take 2 call me in the morning', time_between_dose: 240,
+                     icon: 'path_to_icon')
 
- Prescription.create!(user_id: user.id, med_name: 'OxyContin', time_of_last_dose: Time.current,
-   time_of_next_dose: 4.hours.from_now, total_doses: 25, doses_remaining: 25,
-   max_daily_doses: 4, user_instructions: "['take pill', 'take with food']",
-   doctor_instructions: 'take 2 call me in the morning', time_between_dose: 240,
-   icon: 'path_to_icon'
-   )
+Prescription.create!(user_id: user.id, med_name: 'OxyContin', time_of_last_dose: Time.current,
+                     time_of_next_dose: 4.hours.from_now, total_doses: 25, doses_remaining: 25,
+                     max_daily_doses: 4, user_instructions: "['take pill', 'take with food']",
+                     doctor_instructions: 'take 2 call me in the morning', time_between_dose: 240,
+                     icon: 'path_to_icon')
 
- Prescription.create!(user_id: user.id, med_name: 'Motrin', time_of_last_dose: Time.current,
-   time_of_next_dose: 4.hours.from_now, total_doses: 250, doses_remaining: 250,
-   max_daily_doses: 4, user_instructions: "['take pill', 'take with food']",
-   doctor_instructions: 'take 2 call me in the morning', time_between_dose: 240,
-   icon: 'path_to_icon'
-   )
+Prescription.create!(user_id: user.id, med_name: 'Motrin', time_of_last_dose: Time.current,
+                     time_of_next_dose: 4.hours.from_now, total_doses: 250, doses_remaining: 250,
+                     max_daily_doses: 4, user_instructions: "['take pill', 'take with food']",
+                     doctor_instructions: 'take 2 call me in the morning', time_between_dose: 240,
+                     icon: 'path_to_icon')
