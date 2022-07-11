7ab78c2f94216fa29e351b45c2e1c0f52e328f94
John Hennerich <john.hennerich@gmail.com>
2022-07-11 14:40:44 -0600

    Configure graphiql

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
-  resources :prescriptions
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: 'graphql#execute' if Rails.env.development?
+  post '/graphql', to: 'graphql#execute'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
