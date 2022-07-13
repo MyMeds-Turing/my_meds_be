ad1e290f444b9165659f7570e9995d7ba6d159a2
John Hennerich <john.hennerich@gmail.com>
2022-07-13 17:17:00 -0600

    update seed data for FE

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,30 +25,30 @@ user2 = User.create!(first_name: 'Paul', last_name: 'M', email: 'Paul.M@email.co
 
 Prescription.create!(user_id: user.id, med_name: 'Tylenol', time_of_last_dose: Time.current,
                      time_of_next_dose: 1.days.from_now, total_doses: 500, doses_remaining: 500,
-                     max_daily_doses: 6, dose: '200 mg', user_instructions: "'take pill', 'take with food'",
+                     max_daily_doses: 6, dose: '200 mg', user_instructions: "take pill, take with food",
                      additional_instructions: 'take 2 call me in the morning', time_between_dose: 240,
                      icon: 'path_to_icon')
 
 Prescription.create!(user_id: user2.id, med_name: 'Tylenol', time_of_last_dose: Time.current,
                      time_of_next_dose: 4.days.from_now, total_doses: 500, doses_remaining: 500,
-                     max_daily_doses: 6, dose: '200 mg', user_instructions: "'take pill', 'take with food'",
+                     max_daily_doses: 6, dose: '200 mg', user_instructions: "take pill, take with food",
                      additional_instructions: 'take 2 call me in the morning', time_between_dose: 240,
                      icon: 'path_to_icon')
 
 Prescription.create!(user_id: user.id, med_name: 'OxyContin', time_of_last_dose: Time.current,
                      time_of_next_dose: 2.days.from_now, total_doses: 25, doses_remaining: 25,
-                     max_daily_doses: 4, dose: '5mg', user_instructions: "'take pill', 'take with food'",
+                     max_daily_doses: 4, dose: '5mg', user_instructions: "take pill, take with food",
                      additional_instructions: 'take 2 call me in the morning', time_between_dose: 240,
                      icon: 'path_to_icon')
 
 Prescription.create!(user_id: user.id, med_name: 'Motrin', time_of_last_dose: Time.current,
                      time_of_next_dose: 16.hours.from_now, total_doses: 250, doses_remaining: 250,
-                     max_daily_doses: 4, dose: '200mg', user_instructions: "'take pill', 'take with food'",
+                     max_daily_doses: 4, dose: '200mg', user_instructions: "take pill, take with food",
                      additional_instructions: 'take 2 call me in the morning', time_between_dose: 240,
                      icon: 'path_to_icon')
 
 Prescription.create!(user_id: user2.id, med_name: 'Motrin Extra-Strength', time_of_last_dose: Time.current,
                      time_of_next_dose: 16.days.from_now, total_doses: 250, doses_remaining: 250,
-                     max_daily_doses: 4, dose: '500mg', user_instructions: "'take pill', 'take with food'",
+                     max_daily_doses: 4, dose: '500mg', user_instructions: "take pill, take with food",
                      additional_instructions: 'take 2 call me in the morning', time_between_dose: 240,
                      icon: 'path_to_icon')
