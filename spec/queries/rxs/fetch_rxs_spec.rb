bf90415a41b06c21dc4e59557990eaad43e44552
John Hennerich <john.hennerich@gmail.com>
2022-07-13 21:38:23 -0600

    Add spec tests

---
+++ spec/queries/rxs/fetch_rxs_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Types::QueryType do
+  describe 'display users' do
+    it 'can return all prescriptions for a user  ' do
+      user = User.create!(id: 1, first_name: 'John', last_name: 'L', email: 'John.L@email.com',
+                          sms: '5551234567', notify: 3)
+
+      Prescription.create!(user_id: user.id, med_name: 'Tylenol', time_of_last_dose: Time.current,
+                           time_of_next_dose: 1.days.from_now, total_doses: 500, doses_remaining: 500,
+                           max_daily_doses: 6, dose: '200 mg', user_instructions: 'take pill, take with food',
+                           additional_instructions: 'take 2 call me in the morning', time_between_dose: 240,
+                           icon: 'path_to_icon')
+
+
+      result = MyMedsBeSchema.execute(query).as_json
+      expect(result['data']['fetchRxs'].count).to eq(1)
+      expect(result['data']['fetchRxs'][0]['medName']).to eq('Tylenol')
+    end
+  end
+
+  def query
+    <<~GQL
+    {
+      fetchRxs {
+        id
+        medName
+        timeOfLastDose
+        timeOfNextDose
+        totalDoses
+        dosesRemaining
+        maxDailyDoses
+        dose
+        userInstructions
+        additionalInstructions
+        timeBetweenDose
+        icon
+        userId
+        createdAt
+        updatedAt
+      }
+    }
+    GQL
+  end
+end
