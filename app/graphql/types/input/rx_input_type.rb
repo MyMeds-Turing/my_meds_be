4048ff82014c88c57d1ff2dbe514ec4cf429c603
John Hennerich <john.hennerich@gmail.com>
2022-07-17 12:44:50 -0600

    udpates to allow ad and update rx to only require some fields

--- app/graphql/types/input/rx_input_type.rb
+++ app/graphql/types/input/rx_input_type.rb
@@ -3,18 +3,18 @@
 module Types
   module Input
     class RxInputType < Types::BaseInputObject
-      argument :med_name, String
-      argument :time_of_last_dose, GraphQL::Types::ISO8601DateTime
-      argument :time_of_next_dose, GraphQL::Types::ISO8601DateTime
-      argument :total_doses, Integer
-      argument :doses_remaining, Integer
-      argument :max_daily_doses, Integer
-      argument :dose, String
-      argument :user_instructions, String
-      argument :additional_instructions, String
-      argument :time_between_dose, Integer
-      argument :icon, String
-      argument :user_id, Integer
+      argument :med_name, String, required: true
+      argument :time_of_last_dose, GraphQL::Types::ISO8601DateTime, required: false
+      argument :time_of_next_dose, GraphQL::Types::ISO8601DateTime, required: false
+      argument :total_doses, Integer, required: false
+      argument :doses_remaining, Integer, required: false
+      argument :max_daily_doses, Integer, required: false
+      argument :dose, String, required: false
+      argument :user_instructions, String, required: false
+      argument :additional_instructions, String, required: false
+      argument :time_between_dose, Integer, required: false
+      argument :icon, String, required: false
+      argument :user_id, Integer, required: true
     end
   end
 end
