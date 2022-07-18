46cdb6d947976d678199b06acb13d4cf032deebf
Maximilian Wagner <maw3350@gmail.com>
2022-07-18 11:03:58 -0400

    refactor:remove unneeded comments

--- app/graphql/mutations/delete_rx.rb
+++ app/graphql/mutations/delete_rx.rb
@@ -9,13 +9,3 @@ module Mutations
     end
   end
 end
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
-
-    # TODO: define arguments
-    # argument :name, String, required: true
-
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
