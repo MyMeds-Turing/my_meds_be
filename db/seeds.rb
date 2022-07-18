d1811a5f599cf9974ad73fdf6557f700a3639919
John Hennerich <john.hennerich@gmail.com>
2022-07-18 14:41:08 -0600

    Add users to seed for FE

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,16 @@ ActiveRecord::Base.connection.reset_pk_sequence!('users')
 Notification.create!(recipient: 'John', sent_at: Time.now, notification_type: 'sms')
 Notification.create!(recipient: 'John H', sent_at: Time.now, notification_type: 'email')
 
-user = User.create!(first_name: 'John', last_name: 'L', email: 'John.H@email.com',
+user = User.create!(first_name: 'John', last_name: 'Lennon', email: 'John.Lennon@email.com',
                     sms: '5551234567', notify: 3)
 
-user2 = User.create!(first_name: 'Paul', last_name: 'M', email: 'Paul.M@email.com',
+user2 = User.create!(first_name: 'Paul', last_name: 'McCartney', email: 'Paul.McCartney@email.com',
+                     sms: '5551234567', notify: 3)
+
+user3 = User.create!(first_name: 'George', last_name: 'Harrison', email: 'George.Harrison@email.com',
+                     sms: '5551234567', notify: 3)
+
+user4 = User.create!(first_name: 'Ringo', last_name: 'Starr', email: 'Ringo.Starr@email.com',
                      sms: '5551234567', notify: 3)
 
 Prescription.create!(user_id: user.id, med_name: 'Tylenol', time_of_last_dose: Time.current,
@@ -30,7 +36,13 @@ Prescription.create!(user_id: user.id, med_name: 'Tylenol', time_of_last_dose: T
                      icon: 'path_to_icon')
 
 Prescription.create!(user_id: user2.id, med_name: 'Tylenol', time_of_last_dose: Time.current,
-                     time_of_next_dose: 4.days.from_now, total_doses: 500, doses_remaining: 500,
+                     time_of_next_dose: 1.days.from_now, total_doses: 500, doses_remaining: 500,
+                     max_daily_doses: 6, dose: '200 mg', user_instructions: 'take pill, take with food',
+                     additional_instructions: 'take 2 call me in the morning', time_between_dose: 240,
+                     icon: 'path_to_icon')
+
+Prescription.create!(user_id: user3.id, med_name: 'Tylenol', time_of_last_dose: Time.current,
+                     time_of_next_dose: 1.days.from_now, total_doses: 500, doses_remaining: 500,
                      max_daily_doses: 6, dose: '200 mg', user_instructions: 'take pill, take with food',
                      additional_instructions: 'take 2 call me in the morning', time_between_dose: 240,
                      icon: 'path_to_icon')
