44e743f952a50db399213c18ef70396f156a43ad
Maximilian Wagner <maw3350@gmail.com>
2022-07-18 11:04:23 -0400

    test: update deleteRx spec with passing tests

--- spec/mutations/rxs/delete_rx_spec.rb
+++ spec/mutations/rxs/delete_rx_spec.rb
@@ -10,18 +10,18 @@ RSpec.describe Types::MutationType do
                            time_of_next_dose: 1.days.from_now, total_doses: 500, doses_remaining: 500,
                            max_daily_doses: 6, dose: '200 mg', user_instructions: 'take pill, take with food',
                            additional_instructions: 'take 2 call me in the morning', time_between_dose: 240,
-                           icon: 'path_to_icon')
+                           icon: 'path_to_icon', id: 1)
 
       result = MyMedsBeSchema.execute(mutation).as_json
-      binding.pry
+
       expect(result['data']['deleteRx'].count).to eq(1)
-      expect(result['data']['fetchRxs'][0]['medName']).to eq('Tylenol')
+      expect(result['data']['deleteRx']['id']).to eq('1')
     end
   end
 
   def mutation
     <<~GQL
-      {
+      mutation {
         deleteRx(input: {id: 1}  ) {
           id
         }
