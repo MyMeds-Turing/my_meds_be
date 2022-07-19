c27faa417d03e2059a5eebd92180fd1b7a38ee1a
Maximilian Wagner <maw3350@gmail.com>
2022-07-19 18:39:05 -0400

    feat: add takeMed mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -6,5 +6,6 @@ module Types
     field :add_notification, mutation: Mutations::AddNotification
     field :add_rx, mutation: Mutations::AddRx
     field :update_rx, mutation: Mutations::UpdateRx
+    field :take_med, mutation: Mutations::TakeMed
   end
 end
