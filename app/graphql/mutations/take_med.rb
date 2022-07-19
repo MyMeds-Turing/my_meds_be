eec81741c39c5133e17e3015772f2abcc9af769d
John Hennerich <john.hennerich@gmail.com>
2022-07-19 17:06:54 -0600

    Update takeRx to handle nil case, rubocop updates

--- app/graphql/mutations/take_med.rb
+++ app/graphql/mutations/take_med.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Mutations
   class TakeMed < Mutations::BaseMutation
     argument :id, ID, required: true
@@ -6,15 +8,18 @@ module Mutations
 
     def resolve(attributes)
       rx = Prescription.where(id: attributes[:id]).first
-      time = Time.now
-      attributes[:time_of_last_dose] = time
-      attributes[:time_of_next_dose] = time + rx.time_between_dose.minutes
-      attributes[:doses_remaining] = rx.doses_remaining - 1
-      rx.update(attributes)
-      rx
-      #call the mailer
-      #call the sms
-      #Notification.create .....
+      unless rx.nil?
+        time = Time.now
+        attributes[:time_of_last_dose] = time
+        attributes[:time_of_next_dose] = time + rx.time_between_dose.minutes
+        attributes[:doses_remaining] = rx.doses_remaining - 1
+        rx.update(attributes)
+        binding.pry
+        rx
+      end
+      # call the mailer
+      # call the sms
+      # Notification.create .....
     end
   end
 end
--- spec/mutations/medications/take_med_spec.rb
+++ spec/mutations/medications/take_med_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Types::MutationType do
@@ -7,22 +9,22 @@ RSpec.describe Types::MutationType do
                           sms: '2223334444', notify: 3)
 
       rx = Prescription.create!(user_id: user.id, med_name: 'Tylenol', time_of_last_dose: Time.current,
-                           time_of_next_dose: 1.days.from_now, total_doses: 500, doses_remaining: 500,
-                           max_daily_doses: 6, dose: '200 mg', user_instructions: 'take pill, take with food',
-                           additional_instructions: 'take 2 call me in the morning', time_between_dose: 240,
-                           icon: 'path_to_icon', id: 1)
+                                time_of_next_dose: 1.days.from_now, total_doses: 500, doses_remaining: 500,
+                                max_daily_doses: 6, dose: '200 mg', user_instructions: 'take pill, take with food',
+                                additional_instructions: 'take 2 call me in the morning', time_between_dose: 240,
+                                icon: 'path_to_icon', id: 1)
 
       expect(rx.doses_remaining).to eq(500)
       result = MyMedsBeSchema.execute(mutation).as_json
 
-      expect(result['data']['takeMed']["dosesRemaining"]).to eq(499)
+      expect(result['data']['takeMed']['dosesRemaining']).to eq(499)
       expect(result['data']['takeMed'].keys.count).to eq(5)
       expect(result['data']['takeMed']['id']).to eq('1')
-      expect(result['data']['takeMed']).to have_key("id")
-      expect(result['data']['takeMed']).to have_key("medName")
-      expect(result['data']['takeMed']).to have_key("timeOfLastDose")
-      expect(result['data']['takeMed']).to have_key("timeOfNextDose")
-      expect(result['data']['takeMed']).to have_key("dosesRemaining")
+      expect(result['data']['takeMed']).to have_key('id')
+      expect(result['data']['takeMed']).to have_key('medName')
+      expect(result['data']['takeMed']).to have_key('timeOfLastDose')
+      expect(result['data']['takeMed']).to have_key('timeOfNextDose')
+      expect(result['data']['takeMed']).to have_key('dosesRemaining')
     end
   end
 
