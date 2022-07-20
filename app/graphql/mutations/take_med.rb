6bbac86d0c61f0b5b38ec6fe864899bd3c068fbd
John Hennerich <john.hennerich@gmail.com>
2022-07-19 20:42:35 -0600

    Bug: add logic to prevent negative doses_remaining

--- app/graphql/mutations/take_med.rb
+++ app/graphql/mutations/take_med.rb
@@ -12,7 +12,13 @@ module Mutations
         time = Time.now
         attributes[:time_of_last_dose] = time
         attributes[:time_of_next_dose] = time + rx.time_between_dose.minutes
-        attributes[:doses_remaining] = rx.doses_remaining - 1
+
+        if rx.doses_remaining >= 1
+          attributes[:doses_remaining] = rx.doses_remaining - 1
+        else
+          attributes[:doses_remaining] = 0
+        end
+        
         rx.update(attributes)
         rx
       end
