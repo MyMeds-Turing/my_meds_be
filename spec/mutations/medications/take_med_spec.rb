65adde40db1c0fb74055e2f8e057e5817b35cce2
John Hennerich <john.hennerich@gmail.com>
2022-07-25 17:03:39 -0600

    Update takeMed spec test to use user with no notifications

--- spec/mutations/medications/take_med_spec.rb
+++ spec/mutations/medications/take_med_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Types::MutationType do
   describe 'takeMed' do
     it 'updates all timed attributes for rx and subtracts one from total count' do
       user = User.create!(id: 1, first_name: 'test', last_name: 'man', email: 'guy@test.com',
-                          sms: '2223334444', notify: 3)
+                          sms: '2223334444', notify: 0)
 
       rx = Prescription.create!(user_id: user.id, med_name: 'Tylenol', time_of_last_dose: Time.current,
                                 time_of_next_dose: 1.days.from_now, total_doses: 500, doses_remaining: 500,
